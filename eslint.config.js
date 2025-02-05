7ca721c1a023a5734c4b604cd911c0d3ce009dfb
Vlad Mashkautsan <vladik.mashk@gmail.com>
2025-02-05 10:26:22 +0100

    Add Vite configuration

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import js from "@eslint/js"
+import globals from "globals"
+import reactHooks from "eslint-plugin-react-hooks"
+import reactRefresh from "eslint-plugin-react-refresh"
+import reactCompiler from "eslint-plugin-react-compiler"
+import tseslint from "typescript-eslint"
+
+export default tseslint.config(
+    {ignores: ["dist"]},
+    {
+        extends: [js.configs.recommended, ...tseslint.configs.recommended],
+        files: ["**/*.{ts,tsx}"],
+        languageOptions: {
+            ecmaVersion: "latest",
+            globals: globals.browser,
+        },
+        plugins: {
+            "react-hooks": reactHooks,
+            "react-refresh": reactRefresh,
+            "react-compiler": reactCompiler,
+        },
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+            "react-refresh/only-export-components": [
+                "warn",
+                {allowConstantExport: true},
+            ],
+            "react-compiler/react-compiler": "error",
+            "@typescript-eslint/no-unused-vars": "off"
+        },
+    },
+);
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import {defineConfig} from "vite"
+import react from "@vitejs/plugin-react"
+import checker from "vite-plugin-checker";
+
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [
+        react(),
+        checker({typescript: {tsconfigPath: "tsconfig.app.json"}})
+    ],
+    server: {
+        host: true
+    }
+});
